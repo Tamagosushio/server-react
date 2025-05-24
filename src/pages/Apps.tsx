6970cb96d029250e50035cfeb9216f37e35b8dad
tamagosushio <mitachi.ts75@gmail.com>
2025-05-24 11:12:22 +0900

    [add] 自作言語テストページへの誘導を追加

--- src/pages/Apps.tsx
+++ src/pages/Apps.tsx
@@ -7,14 +7,28 @@ function Apps() {
       <p>
         作ったwebアプリを雑多に置きます。
       </p>
-      <h3>
-        <a href="/apps/procon34-visualizer-web/index.html" className="text-decoration-none">
-          第34回高専プロコン競技部門再現
-        </a>
-      </h3>
-      <p>
-        公式サイトで公開されている操作データを元に、実際の試合の様子を再現したものです。<br/>
-      </p>
+      <div>
+        <h3>
+          <a href="/apps/procon34-visualizer-web/index.html" className="text-decoration-none">
+            第34回高専プロコン競技部門再現
+          </a>
+        </h3>
+        <p>
+          公式サイトで公開されている操作データを元に、実際の試合の様子を再現したものです。<br/>
+        </p>
+      </div>
+      <div>
+        <h3>
+          <a href="/apps/programlingvo/index.html" className="text-decoration-none">
+            自作言語「Programlingvo」のデモ
+          </a>
+        </h3>
+        <p>
+          <a href="/articles/myLanguage/programlingvo1" className="text-decoration-none">
+            この自作言語を作る記事
+          </a>で作成した言語をテストできます。<br/>
+        </p>
+      </div>
     </div>
   );
 }
